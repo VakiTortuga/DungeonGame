974d528c39567a2d0f109d537b82bde2c069c5c3
AndrewG <andrey_g.2019@mail.ru>
2025-12-23 01:13:44 +0700

    Добавить карту

---
+++ DungeonGame/src/Game.Core/MapObject/Interfaces/IMapToEntity.cs
@@ -0,0 +1,19 @@
+﻿using DungeonGame.src.Game.Application.enumerations;
+using DungeonGame.src.Game.Core.Cell.Interfaces;
+using DungeonGame.src.Game.Core.enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.src.Game.Core.MapObject.Interfaces
+{
+    internal interface IMapToEntity
+    {
+        bool TryMoveEntity(ICellToMap from, ICellToMap to);
+        ICellToMap GetCellByDirection(ICellToMap cell, FacingDirection direction);
+        ICellToMap GetCell(int x, int y);
+        (int width, int height) GetDimensions();
+    }
+}
---
+++ DungeonGame/src/Game.Core/MapObject/MapObject.cs
@@ -0,0 +1,89 @@
+﻿using DungeonGame.src.Game.Application.enumerations;
+using DungeonGame.src.Game.Core.Cell.Interfaces;
+using DungeonGame.src.Game.Core.Cell;
+using DungeonGame.src.Game.Core.MapObject.Interfaces;
+using DungeonGame.src.Game.Core.enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.src.Game.Core.MapObject
+{
+    internal class MapObject : IMapToEntity
+    {
+        private readonly CellObject[,] cells;
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public MapObject(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            cells = new CellObject[width, height];
+
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    cells[x, y] = new CellObject(x, y, this);
+        }
+
+        public (int width, int height) GetDimensions()
+            => (Width, Height);
+
+        public ICellToMap GetCell(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= Width || y >= Height)
+                return null;
+
+            return cells[x, y];
+        }
+
+        public ICellToMap GetCellByDirection(
+            ICellToMap cell,
+            FacingDirection direction)
+        {
+            int x = cell.X;
+            int y = cell.Y;
+
+            ICellToMap result;
+            switch (direction)
+            {
+                case FacingDirection.Up:
+                    result = GetCell(x, y - 1);
+                    break;
+                case FacingDirection.Down:
+                    result = GetCell(x, y + 1);
+                    break;
+                case FacingDirection.Left:
+                    result = GetCell(x - 1, y);
+                    break;
+                case FacingDirection.Right:
+                    result = GetCell(x + 1, y);
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+            return result;
+        }
+
+        public bool TryMoveEntity(ICellToMap from, ICellToMap to)
+        {
+            if (from == null || to == null)
+                return false;
+
+            if (!from.IsOccupied || to.IsOccupied)
+                return false;
+
+            var entity = from.GetEntity();
+
+            from.RemoveEntity();
+            to.PlaceEntity(entity);
+
+            return true;
+        }
+    }
+}
