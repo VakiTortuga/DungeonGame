9830bb3e16ef22d387307ec02e074ee12ee5678a
AndrewG <andrey_g.2019@mail.ru>
2025-12-22 22:26:16 +0700

    Добавить перечислимые типы данных, реализация Entity, файлы интерфейсов клетки и карты

---
+++ DungeonGame/src/Game.Core/ICellToEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.src.Game.Core
+{
+    internal interface ICellToEntity
+    {
+        int X { get; }
+        int Y { get; }
+        bool IsOccupied { get; }
+        Entity GetEntity();
+        IMapToEntity getMap();
+    }
+}
---
+++ DungeonGame/src/Game.Core/enumerations/FacingDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.src.Game.Core.enumerations
+{
+    internal enum FacingDirection
+    {
+        None,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
