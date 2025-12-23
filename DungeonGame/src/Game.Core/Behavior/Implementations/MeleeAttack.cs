f24ab8553cb10754aa0fc13139b16853006a64ab
AndrewG <andrey_g.2019@mail.ru>
2025-12-23 15:39:24 +0700

    Добавить реализацию поведения MeleeAttack

--- DungeonGame/src/Game.Core/Behavior/Implementations/MeleeAttack.cs
+++ DungeonGame/src/Game.Core/Behavior/Implementations/MeleeAttack.cs
@@ -1,4 +1,5 @@
 ﻿using DungeonGame.src.Game.Core.BehaviorInterfaces;
+using DungeonGame.src.Game.Core.enumerations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,26 @@ namespace DungeonGame.src.Game.Core.BehaviorImplementations
 
         public bool TryAttack(Entity attacker)
         {
-            throw new NotImplementedException();
+            var currentCell = attacker.Location;
+            var map = currentCell.GetMap();
+            var direction = attacker.FacingDirection;
+            var targetCell = map.GetCellByDirection(currentCell, direction);
+
+            if (targetCell == null || !targetCell.IsOccupied)
+                return false;
+
+            var targetEntity = targetCell.GetEntity();
+
+            // Проверка, можно ли атаковать эту сущность
+            if (targetEntity.EntityType == EntityType.Enemy ||
+                targetEntity.EntityType == EntityType.Player)
+            {
+                // Нанести урон
+                targetEntity.TakeDamage(10); // Примерный урон
+                return true;
+            }
+
+            return false;
         }
     }
 }
