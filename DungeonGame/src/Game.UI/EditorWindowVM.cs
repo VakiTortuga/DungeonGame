5d0a573787dcbb2a27375ae703cbadf1ad614b65
KAGS <sergej.naumov.0606@mail.ru>
2025-12-23 21:59:50 +0700

    Добавить главное окно игры

---
+++ DungeonGame/src/Game.UI/EditorWindowVM.cs
@@ -0,0 +1,63 @@
+﻿using DungeonGame.src.Game.Application.DTOs;
+using DungeonGame.src.Game.Application.enumerations;
+using DungeonGame.src.Game.Application.ServiceInterfaces;
+using DungeonGame.src.UI.Commands;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace DungeonGame.src.UI.ViewModels
+{
+    internal class EditorWindowVM : BaseViewModel
+    {
+        private readonly ILevelEditorService _editorService;
+
+        private EditorStateDTO _state;
+        public EditorStateDTO State
+        {
+            get { return _state; }
+            private set
+            {
+                _state = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<AvailableEntityDTO> AvailableEntities { get; }
+
+        public ICommand PlaceEntityCommand { get; }
+        public ICommand RemoveEntityCommand { get; }
+        public ICommand SaveLevelCommand { get; }
+
+        public EditorWindowVM(ILevelEditorService editorService)
+        {
+            _editorService = editorService;
+
+            State = _editorService.GetEditorState();
+            AvailableEntities = new ObservableCollection<AvailableEntityDTO>(
+                _editorService.GetAvailableEntities());
+
+            PlaceEntityCommand = new RelayCommand(p =>
+            {
+                var data = (Tuple<int, int>)p;
+                State = _editorService.PlaceEntity(
+                    data.Item1,
+                    data.Item2,
+                    State.SelectedEntityType);
+            });
+
+            RemoveEntityCommand = new RelayCommand(p =>
+            {
+                var data = (Tuple<int, int>)p;
+                State = _editorService.RemoveEntity(
+                    data.Item1,
+                    data.Item2);
+            });
+
+            SaveLevelCommand = new RelayCommand(p =>
+            {
+                _editorService.SaveLevelAs((string)p);
+            });
+        }
+    }
+}
---
+++ DungeonGame/src/Game.UI/PauseMenuVM.cs
@@ -0,0 +1,29 @@
+﻿using DungeonGame.src.Game.Application.ServiceInterfaces;
+using DungeonGame.src.UI.Commands;
+using System.Windows.Input;
+
+namespace DungeonGame.src.UI.ViewModels
+{
+    internal class PauseMenuVM : BaseViewModel
+    {
+        private readonly IGameSessionService _gameService;
+
+        public ICommand ResumeCommand { get; }
+        public ICommand SaveAndExitCommand { get; }
+
+        public PauseMenuVM(IGameSessionService gameService)
+        {
+            _gameService = gameService;
+
+            ResumeCommand = new RelayCommand(_ =>
+            {
+                _gameService.ResumeGame();
+            });
+
+            SaveAndExitCommand = new RelayCommand(_ =>
+            {
+                _gameService.SaveAndExit();
+            });
+        }
+    }
+}
