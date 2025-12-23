4d2d07268c79de9bb42334904bca8112a8388ae7
KAGS <sergej.naumov.0606@mail.ru>
2025-12-23 23:45:35 +0700

    Добавить интерфейс AppState

---
+++ DungeonGame/src/Game.Application/Services/AppState.cs
@@ -0,0 +1,14 @@
+﻿using DungeonGame.src.Game.Application.enumerations;
+using System;
+
+namespace DungeonGame.src.Game.Application.DTOs
+{
+    public class AppState
+    {
+        public ApplicationState CurrentState { get; set; }
+        public GameStateDTO GameState { get; set; }
+        public EditorStateDTO EditorState { get; set; }
+        public string ErrorMessage { get; set; }
+        public string SuccessMessage { get; set; }
+    }
+}
