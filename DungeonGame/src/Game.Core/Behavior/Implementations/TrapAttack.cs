f22b23471efb211fda420a0ed16239d7f097312a
AndrewG <andrey_g.2019@mail.ru>
2025-12-23 15:39:51 +0700

    Добавить реализацию поведения TrapAttack

--- DungeonGame/src/Game.Core/Behavior/Implementations/TrapAttack.cs
+++ DungeonGame/src/Game.Core/Behavior/Implementations/TrapAttack.cs
@@ -1,4 +1,6 @@
-﻿using DungeonGame.src.Game.Core.BehaviorInterfaces;
+﻿using DungeonGame.src.Game.Application.enumerations;
+using DungeonGame.src.Game.Core.BehaviorInterfaces;
+using DungeonGame.src.Game.Core.enumerations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,39 @@ namespace DungeonGame.src.Game.Core.BehaviorImplementations
 
         public bool TryAttack(Entity attacker)
         {
-            throw new NotImplementedException();
+            // Ловушка атакует все сущности на соседних клетках
+            var currentCell = attacker.Location;
+            var map = currentCell.GetMap();
+
+            bool attacked = false;
+
+            // Проверяем все 4 направления
+            var directions = new[] {
+        FacingDirection.Up,
+        FacingDirection.Down,
+        FacingDirection.Left,
+        FacingDirection.Right
+    };
+
+            foreach (var direction in directions)
+            {
+                var targetCell = map.GetCellByDirection(currentCell, direction);
+
+                if (targetCell != null && targetCell.IsOccupied)
+                {
+                    var targetEntity = targetCell.GetEntity();
+
+                    // Ловушка может атаковать игрока и врагов
+                    if (targetEntity.EntityType == EntityType.Player ||
+                        targetEntity.EntityType == EntityType.Enemy)
+                    {
+                        targetEntity.TakeDamage(5); // Урон от ловушки
+                        attacked = true;
+                    }
+                }
+            }
+
+            return attacked;
         }
     }
 }
