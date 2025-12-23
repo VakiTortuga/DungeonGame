4c0e9a5fb0e61a15ea4801544c5670762e2d846d
AndrewG <andrey_g.2019@mail.ru>
2025-12-23 21:07:20 +0700

    Добавить DTO для сохранения состояния игры

---
+++ DungeonGame/src/Game.Infrastructure/DTOs/EntitySaveDTO.cs
@@ -0,0 +1,21 @@
+﻿using DungeonGame.src.Game.Application.enumerations;
+using DungeonGame.src.Game.Core.enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonGame.src.Game.Infrastructure.DTOs
+{
+    [Serializable]
+    internal class EntitySaveDTO
+    {
+        public int Id;
+        public EntityType Type;
+        public int X;
+        public int Y;
+        public int Health;
+        public FacingDirection Facing;
+    }
+}
